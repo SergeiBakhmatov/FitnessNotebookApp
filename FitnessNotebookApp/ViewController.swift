c5ba5784d8b9878aa1145b6a6b2bf1eab44b0a81
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-07-21 16:16:36 +0500

    Initial Commit

---
+++ FitnessNotebookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessNotebookApp
+//
+//  Created by Sergei Bakhmatov on 21.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
