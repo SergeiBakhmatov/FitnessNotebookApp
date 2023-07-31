0222bef1f1c7078c0566c29b249250dce23e4d61
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-07-31 13:50:30 +0500

    Dev

--- FitnessNotebookApp/WorkoutList/TestViewController.swift
+++ FitnessNotebookApp/WorkoutList/TestViewController.swift
@@ -59,7 +59,6 @@ class TestViewController: UIViewController, UITableViewDataSource, UITableViewDe
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .blue
-//        cell.configurationState
         return cell
     }
     
